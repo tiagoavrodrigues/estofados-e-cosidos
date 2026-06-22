33210bc40e7dc37835fd09393338577de9469e63
Tiago Rodrigues <tiagoalexandre@ipvc.pt>
2026-06-22 23:09:22 +0100

    chore: initial project setup

---
+++ src/main/java/com/example/estofadosecosidos/EstofadosecosidosApplication.java
@@ -0,0 +1,13 @@
+package com.example.estofadosecosidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstofadosecosidosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstofadosecosidosApplication.class, args);
+    }
+
+}
